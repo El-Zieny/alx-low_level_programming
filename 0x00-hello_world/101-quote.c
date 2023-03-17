00cffbe27735e2f838880e746434cd9917700629
El-Zieny <abdoelzieny72@gmail.com>
2023-03-16 19:59:45 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	putchar(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1)
+}
