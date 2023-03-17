7e0e2f406e1cad2aa2ca8b47284b33024a34b792
El-Zieny <abdoelzieny72@gmail.com>
2023-03-17 15:40:42 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+		for ( m = n + 1; m < 10; m++)
+			putchar('0' + m);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
