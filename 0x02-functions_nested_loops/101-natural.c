c4d1805caee8213a615f6d35bb37965a943817ea
El-Zieny <abdoelzieny72@gmail.com>
2023-03-21 14:14:23 -0700

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,18 @@ int main(void)
 	int t, f;
 	int n, m;
 	int res;
+	int test;
 
 	t = 0;
 	f = 0;
 	for (n = 0; n < 1024; n += 3)
 		t += n;
 	for (m = 0; m < 1024; m += 5)
-		f += m;
+	{
+		test = m % 3;
+		if (test = 0)
+			f += m;
+	}
 	res = t + f;
 	printf("%d\n", res);
 	return (0);
