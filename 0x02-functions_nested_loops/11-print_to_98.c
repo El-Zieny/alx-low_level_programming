b46e1f4f373443d87d64a2563693d9ecf3b6f831
El-Zieny <abdoelzieny72@gmail.com>
2023-03-21 08:28:02 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,26 @@
  */
 void print_to_98(int n)
 {
-	for (; n < 99; n++)
-		printf("%d", n);
+	if (n < 99)
+	{
+		for (; n < 99; n++)
+		{
+			if ( n == 98)
+				printf("%d", n);
+			else
+				printf("%d, ", n);
+		}
+
+	}
+	else
+	{
+		for (; n > 97; n--)
+		{
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d, ", n);
+		}
+	}
 	putchar('\n');
 }
