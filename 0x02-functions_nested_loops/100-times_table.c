2304eb847d66ce69b332438698005315790f0223
El-Zieny <abdoelzieny72@gmail.com>
2023-03-21 12:47:12 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 			if (t > 99)
 			{
 			_putchar(t / 100 + '0');
-			_putchar(t % 100 + '0');
+			_putchar(t % 100 / 10 + '0');
 			_putchar(t % 10 + '0');
 			}
 			else if (t > 9)
