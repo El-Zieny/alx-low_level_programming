00c24d1c7eebcd5821816ab931ebd62d6530be6a
El-Zieny <abdoelzieny72@gmail.com>
2023-03-21 03:00:14 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _isalpha - check the code
+ * c: is it an alphabet or not
+ * Return: 1 if yes 0 of no
+ */
 int _isalpha(int c)
 {
 	if((c > 64 && c < 91) || (c > 96 && c < 123))
