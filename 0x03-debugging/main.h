1abcaf0a53fdb8168b574247cd6891f7166e6c2d
El-Zieny <abdoelzieny72@gmail.com>
2023-03-23 13:47:38 -0700

    3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,5 +9,6 @@ int largest_number(int a, int b, int c);
 
 void print_remaining_days(int month, int day, int year);
 
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
