1010299d00bc157fcd699a643232c176c337862a
El-Zieny <abdoelzieny72@gmail.com>
2023-03-23 11:57:31 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,14 +8,18 @@ void print_triangle(int size)
 	int x;
 	int n, m;
 
-	m = size;
+	if (size > 0)
+	{
 	for (x = 0;x < size; x++)
 	{
+		m = size - x;
 		for (; m > 1; m--)
 			_putchar(' ');
-		m += 1;
-		for (n = 1; n < x; n++)
+		for (n = -1; n < x; n++)
 			_putchar('#');
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
