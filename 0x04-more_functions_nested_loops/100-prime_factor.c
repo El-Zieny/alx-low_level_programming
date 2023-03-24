db14cb4812f9b3e2884c8d75189de69a9db4b507
El-Zieny <abdoelzieny72@gmail.com>
2023-03-23 19:16:21 -0700

    100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, c, res;
+	long int num;
+
+	num = 612852475143 / 2;
+	for (i = 2; i < num; i++)
+	{
+		if ((num % i) == 0)
+		{
+			for ( c = 2; c < (i / 2); c++)
+			{
+				if ((i % c) == 0)
+				{
+					break;
+				}
+				else
+					res = i;
+			}
+		}
+	}
+	printf("%i\n", res);
+	return (0);
+}
