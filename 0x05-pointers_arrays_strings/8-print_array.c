8090374dab6b1b91b5fa09832c6650527f964609
El-Zieny <abdoelzieny72@gmail.com>
2023-03-27 15:02:43 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,8 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%i", a[i]);
-		if (!(n - ( i + 1)))
-				printf(", ");
+		if (n != i + 1)
+			printf(", ");
 	}
+	putchar('\n');
 }
