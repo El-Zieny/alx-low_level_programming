5259e7763712ed0ae952ca5ca06efc857ed1a359
El-Zieny <abdoelzieny72@gmail.com>
2023-03-28 10:30:43 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,40 +7,34 @@
  */
 int _atoi(char *s)
 {
-	int i, l, p, m, c, val;
+	int i, l, m, c, val, min;
 
 	c = 0;
 	p = 0;
 	m = 0;
 	l = strlen(s);
-/**	for (i = 0; i < l; i++)
-	{
-		if (s[i] == '+')
-			p += 1;
-		else if (s[i] == '-')
-			m += 1;
-	}
-	*/
 	val = 0;
+	min = -2147483648;
 	for (i = 0; i < l; i++)
 	{
-		if (s[i] == '+')
-			p += 1;
-		else if (s[i] == '-')
+		if (s[i] == '-')
 			m += 1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			c++;
 			val = val * 10 + (s[i] - '0');
-			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+			if (val == 214748364 && s[i + 1] == 8)
 			{
+				val = min;
 				break;
 			}
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+				break;
 			if ((i + 1) == l)
 				break;
 		}
 	}
-	if (m % 2)
+	if (m % 2 && val != min)
 		val = val * -1;
 	return (val);
 }
