8c145699fd0084ded17272191f0bf7028689f237
El-Zieny <abdoelzieny72@gmail.com>
2023-03-28 07:34:32 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,7 +26,7 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			c++;
-			if ((s[i + 1] >= '0' && s[i + 1] <= '9') || i == l)
+			if ((s[i + 1] >= '0' && s[i + 1] <= '9') || (i + 1) == l)
 			{
 				val = val * 10 + s[i];
 				continue;
