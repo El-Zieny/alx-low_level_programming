29168d90fb5fb82ae1d84ec89bb8f857c71e3ed1
El-Zieny <abdoelzieny72@gmail.com>
2023-03-28 07:21:39 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,6 @@
 int _atoi(char *s)
 {
 	int i, l, p, m, c, val;
-	char res[32];
 
 	c = 0;
 	p = 0;
@@ -21,13 +20,29 @@ int _atoi(char *s)
 		else if (s[i] == '-')
 			m += 1;
 	}
+	val = 0;
 	for (i = 0; i < l; i++)
 	{
 		if (s[i] > 47 && s[i] < 58)
 		{
-			res[c] = s[i];
 			c++;
 			if ((s[i + 1] > 47 && s[i + 1] < 58) || i == l)
+				val = val * 10 + s[i];
+				continue;
+			else
+				break;
+		}
+	}
+	/**
+	 * for (i = 0; i < c; i++)
+	{
+		if (s[i] > 47 && s[i] < 58)
+		{
+			for (n = c; c != 0; c--)
+			{
+				val = val + s[i] * 10;
+			}
+				if ((s[i + 1] > 47 && s[i + 1] < 58) || i == l)
 				continue;
 			else
 				break;
@@ -36,6 +51,7 @@ int _atoi(char *s)
 	val = 0;
 	for (i = 0; i < c; i++)
 		val = val * 10 + (res[i] - '0');
+		*/
 	if (m > p)
 		val = val * -1;
 	return (val);
