e5063a08a1e0697d4c375995d7b10366f5cc7228
El-Zieny <abdoelzieny72@gmail.com>
2023-03-28 11:18:44 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -25,7 +25,7 @@ int _atoi(char *s)
 				break;*/
 				return (min);
 			}
-			if (!(val == 214748364)
+			if (!(val == 214748364))
 				val = val * 10 + (s[i] - '0');
 			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
 				break;
