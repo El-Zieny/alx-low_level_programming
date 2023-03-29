d47ee4fe6c780bd5062e31233e25580ca2273f2f
El-Zieny <abdoelzieny72@gmail.com>
2023-03-29 08:10:52 -0700

    general

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,20 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strcat - checl the code
+ * _strncat - limitable
  * @dest: destination
  * @src: source
- * Return: concatenates
+ * @n: limitation
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int l1, l2, i;
 
 	l1 = strlen(dest);
 	l2 = strlen(src);
-	for (i = 0; i < l2; i++)
+	if (n > l2)
+		n = l2;
+	for (i = 0; i < n; i++)
 	{
 		dest[l1] = src[i];
 		l1++;
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+/**
+ * _strncpy - check the code
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
