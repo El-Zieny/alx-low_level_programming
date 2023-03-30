68f05cd22b85fb7e0c457c83fff9b52db6cfaacb
El-Zieny <abdoelzieny72@gmail.com>
2023-03-30 16:53:23 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include <string.h>
+/**
+ * _strcmp - check the code
+ * @s1:1st string
+ * @s2: string 2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, r, l;
+
+	l = strlen(s1);
+	for (i = 0; i < l; i++)
+	{
+		r = 0;
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (r);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+/**
+ * string_toupper - check the code
+ * @: the string
+ * Return: uppercase string
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; i < INT_MAX; i++)
+
+}
