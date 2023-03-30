086259ba2ff8ba1f5f55518257085ad498d2992c
El-Zieny <abdoelzieny72@gmail.com>
2023-03-30 08:07:34 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,16 +8,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, l, d;
+	int i;
 
-	l = strlen(src);
-	d = strlen(dest);
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		if (src[i])
+			dest[i] = src[i];
 	}
-	if (n <= l)
-		dest[n + 1] = '\0';
-	dest[] = dest[n + 1];
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
