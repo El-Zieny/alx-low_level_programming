4e43b12f7e42c6b07124c9f602cf04f5d898ce6b
El-Zieny <abdoelzieny72@gmail.com>
2023-03-30 11:24:24 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * reverse_array - check the code
+ * @a: the array
+ * n: its length
+ */
+void reverse_array(int *a, int n)
+{
+	int a, tpm, l;
+
+	m = n - 1;
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[m];
+		a[m] = a[i];
+		a[i] = tmp;
+		m--;
+	}
+}
