737de9c4dd6e98fe01d743c416aae8e1d6ae9416
El-Zieny <abdoelzieny72@gmail.com>
2023-03-30 08:21:53 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include <string.h>
 /**
  * _strncpy - check the code
- * @dest: destination
+ * @dest: destinationi
  * @src: source
  * @n: number
  * Return: dest
@@ -10,24 +10,25 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
 	return (dest);
-	/**size_t i;
-
-               for (i = 0; i < n && src[i] != '\0'; i++)
-                   dest[i] = src[i];
-               for ( ; i < n; i++)
-                   dest[i] = '\0';
-
-               return dest;
-	**
-	 * int i;
-
-	for (i = 0; i < n; i++)
-	{
-		if (src[i])
-			dest[i] = src[i];
-	}
-	for (; i < n; i++)
-		dest[i] = '\0';
-	return (dest);
-	*/
+	/**
+	 * size_t i;
+*
+*              for (i = 0; i < n && src[i] != '\0'; i++)
+*                 dest[i] = src[i];
+*            for ( ; i < n; i++)
+*               dest[i] = '\0';
+*
+*              return dest;
+*	**
+*	 * int i;
+*
+*	for (i = 0; i < n; i++)
+*	{
+*		if (src[i])
+*			dest[i] = src[i];
+*	}
+*	for (; i < n; i++)
+*		dest[i] = '\0';
+*	return (dest);
+*	*/
 }
