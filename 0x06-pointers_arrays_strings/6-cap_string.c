64f0a0bd4742267e7473f39507570b76e0beba60
El-Zieny <abdoelzieny72@gmail.com>
2023-03-30 18:09:44 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include <string.h>
+/**
+ * cap_string - check the code
+ * @: the string
+ * Return: cap-ed string
+ */
+char *cap_string(char *s)
+{
+	char sprt[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int l, i, x;
+
+	l = strlen(s);
+	for (i = 0; i < l; i++)
+	{
+		for (x = 0; x < 13; x++)
+		{
+			if (s[i] == sprt[x])
+			{
+				if (s[i + 1] > 'a' && s[i + 1] < 'z')
+					s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		
+	}
+	return (s);
+}
