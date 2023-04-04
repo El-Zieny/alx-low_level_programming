ee78e94172dfabc57d2a00f4f5fbd2cca525e33f
El-Zieny <abdoelzieny72@gmail.com>
2023-04-03 17:41:59 -0700

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,8 +9,8 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, x, n, val, l1, l2;
 
-	if (needle == '\0')
-		return (&haystack);
+	if (*needle == 0)
+		return (haystack);
 	l1 = strlen(haystack);
 	l2 = strlen(needle);
 	for (i = 0; i < l1; i++)
