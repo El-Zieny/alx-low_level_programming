15f92d09d1914ad5fae6b23feb65eed72b60d76a
El-Zieny <abdoelzieny72@gmail.com>
2023-04-05 19:55:09 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,22 @@
 /**
+ * sqr - instead of the loop, a double parameters func
+ * @n: the same
+ * @i: check for answer
+ * Return: the root
+ */
+int sqr(int n, int i)
+{
+	int sq = i * i;
+
+	if (sq > n)
+		return (-1);
+	if (sq == n)
+		return (i);
+	return sqr(n, i + 1);
+}
+
+
+/**
  * _sqrt_recursion - square root for n
  * @n: the num
  * Return: the root result
@@ -7,5 +25,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (1);
+	return (sqr(n, 1));
 }
