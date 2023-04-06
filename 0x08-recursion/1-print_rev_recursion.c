6e26289b855863be67aa9293e4a63d5a2e4825a7
El-Zieny <abdoelzieny72@gmail.com>
2023-04-05 18:15:35 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _Print_rev_recursion - check the code
+ * @s: the string to be reversed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(++s);
+	_putchar(*--s);
+}
