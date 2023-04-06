2e06078fe34409891d1fe460c48c0c9929da7982
El-Zieny <abdoelzieny72@gmail.com>
2023-04-06 02:46:12 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,27 @@
 /**
+ * _test - testing
+ * @n: is prime
+ * @i: tester
+ * Return: result
+ */
+int _test(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return(_test(n, i - 1));
+}
+/**
  * is_prime_number - check the code
  * @n: testing it
  * @Return: 1 if prime, else 0
  */
 int is_prime_number(int n)
 {
-	if (n > 0)
+	if (n == 2)
 		return (1);
-	return (0);
+	if (n < 2 || n % 2 == 0)
+		return (0);
+	return (_test(n, n / 2));
 }
