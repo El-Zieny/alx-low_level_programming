34b8cb7d981b7a8a2fe71fceef7e667184a45fb3
El-Zieny <abdoelzieny72@gmail.com>
2023-04-05 19:34:15 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _sqrt_recursion - square root for n
+ * @n: the num
+ * Return: the root result
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (n < 0)
+		return (-1);
+	i = 1;
+	if 
+}
