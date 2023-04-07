73b655c3b68598d28832c15f2ae7541ee34de9f1
El-Zieny <abdoelzieny72@gmail.com>
2023-04-06 19:46:28 -0700

    100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+/**
+ * is_palindrome - check if a string is a palindrome or not
+ * @s: the string to be checked
+ * Return: 1 if palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = _strlen(s);
+	if (l % 2)
+		return (1);
+	return (1);
+}
