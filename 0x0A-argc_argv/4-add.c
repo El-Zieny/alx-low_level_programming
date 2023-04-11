35a16a7c0d517521499eb139ee9919f7c245abad
El-Zieny <abdoelzieny72@gmail.com>
2023-04-10 20:00:06 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,17 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
+	(void) argc;
 	(void) argv;
-	if (argc == 1)
-		printf("0\n");
-	for (i = 0; i < argc; i++)
-	{
-		if (!(argv[i] >= 0 && argv[i] <= 9))
-		{
-			printf("Error\n");
-		}
-	}
 	return (0);
 }
