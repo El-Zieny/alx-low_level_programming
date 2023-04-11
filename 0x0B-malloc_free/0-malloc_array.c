0e18334a37c9d00228fbf81d170623ffdeebbbd3
El-Zieny <abdoelzieny72@gmail.com>
2023-04-11 12:56:04 -0700

    0

---
+++ 0x0B-malloc_free/0-malloc_array.c
@@ -0,0 +1,10 @@
+/**
+ * create_array - check the code
+ * @size: size
+ * @c: charachter
+ * Return: pointer to array or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	return ('\0');
+}
