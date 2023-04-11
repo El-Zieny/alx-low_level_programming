9caba701f53dd35f689d9fe17049396e291c90f1
El-Zieny <abdoelzieny72@gmail.com>
2023-04-11 13:21:12 -0700

    3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+/**
+ * alloc_grid - check the code
+ * @width: true
+ * @height: true
+ * Return: NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	(void) width;
+	(void) height;
+	return ('\0');
+}
