3bf31a70d471e57d5d384f72dc466cd3994e5fe1
El-Zieny <abdoelzieny72@gmail.com>
2023-04-12 04:36:41 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /**
  * _strdup - check the code
  * @str: string
@@ -5,6 +6,13 @@
  */
 char *_strdup(char *str)
 {
-	(void) str;
-	return ('\0');
+	char *dup;
+	int i;
+
+	if (!str)
+		return ('\0');
+	dup = malloc(sizeof(str) * strlen(str));
+	for (i = 0; i < strlen(str); i++)
+		dup[i] = str[i];
+	return (dup);
 }
