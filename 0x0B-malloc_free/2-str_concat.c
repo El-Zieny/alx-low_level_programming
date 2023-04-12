6c96063c2437e03bdd1acdc70584c3a93f9cccf4
El-Zieny <abdoelzieny72@gmail.com>
2023-04-12 04:48:42 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /**
  * str_concat - check the code
  * @s1: s1
@@ -6,7 +7,15 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	(void) s1;
-	(void) s2;
-	return ('\0');
+	char *conc;
+	int i;
+
+	conc = malloc(strlen(s1) + strlen(s2));
+	if (!conc)
+		return ('\0');
+	for (i = 0; i < strlen(s1); i++)
+		conc[i] = s1[i];
+	for (i = strlen(s1); i < strlen(s1) + strlen(s2); i++)
+		conc[i] = s2[i];
+	return (conc);
 }
