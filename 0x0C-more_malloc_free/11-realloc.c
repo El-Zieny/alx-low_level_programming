792f54c58b4b808125c61a1aaf6e211608f6397a
El-Zieny <abdoelzieny72@gmail.com>
2023-04-13 18:21:07 -0700

    3

---
+++ 0x0C-more_malloc_free/11-realloc.c
@@ -0,0 +1,13 @@
+/**
+ * _realloc - check the code
+ * @ptr: pointer
+ * @old_size: yes
+ * @new_size: yes
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	(void) ptr;
+	(void) old_size;
+	(void) new_size;
+	return ('\0');
+}
