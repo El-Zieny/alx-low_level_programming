0500bf778117e6c330fb35cc38b5902d80bbd1df
El-Zieny <abdoelzieny72@gmail.com>
2023-04-13 18:25:44 -0700

    3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,6 +5,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int i, x;
+	char *hh;
+
+	i = 0;
+	x = 1;
+	hh = malloc(22);
+	if (!hh)
+		return ('\0');
+	if (i > x)
+		return (hh);
 	(void) nmemb;
 	(void) size;
 	return ('\0');
