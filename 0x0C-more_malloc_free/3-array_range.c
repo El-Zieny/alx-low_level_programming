4515f46caf2117226ab8428c45f241a2c162aa43
El-Zieny <abdoelzieny72@gmail.com>
2023-04-14 20:26:51 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * array_range - check the code
  * @min: minimum
@@ -6,7 +7,20 @@
  */
 int *array_range(int min, int max)
 {
-	(void) min;
-	(void) max;
-	return ('\0');
+	int *arr, n, i;
+
+	if (min > max)
+		return ('\0');
+	arr = malloc(sizeof(int) * (max - min + 1));
+	if (!arr)
+		return('\0');
+	n = min;
+	i = 0;
+	while (n <= max)
+	{
+		arr[i] = n;
+		n++;
+		i++;
+	}
+	return (arr);
 }
