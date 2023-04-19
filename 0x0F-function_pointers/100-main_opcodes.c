bc51a027712e7b8962e388277397b4cb4746a4e2
El-Zieny <abdoelzieny72@gmail.com>
2023-04-19 09:10:51 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - check the code
  */
-void main(void)
+void main(int argc, char *argv)
 {
+	if (argc > 1)
+	{
+		printf("Error\n");
+		exit(1);
+	if (argv[0] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 }
