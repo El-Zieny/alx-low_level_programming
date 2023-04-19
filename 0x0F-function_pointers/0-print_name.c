8f606671564c6cfe21ba5e3e0432c4778dbede78
El-Zieny <abdoelzieny72@gmail.com>
2023-04-19 04:19:58 -0700

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,11 @@
  * print_name - prints a name
  * @name: the name
  * @f: pointer to function
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name)
+	if (!name || !f)
 		return;
 	f(name);
 }
