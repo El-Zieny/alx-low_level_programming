db011eaec2abf6461e620eaf7e13ed2ed4581e2f
El-Zieny <abdoelzieny72@gmail.com>
2023-04-19 04:59:38 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,12 +9,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || !cmp || !arr
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
-	return(-1);
+	return (-1);
 }
