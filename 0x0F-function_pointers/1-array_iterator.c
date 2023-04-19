6577460367213cdd3d72ccb3d17820573d231160
El-Zieny <abdoelzieny72@gmail.com>
2023-04-19 04:34:47 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,12 +3,14 @@
  * array_iterator - whatever
  * @array: array
  * @size: it's size
- * @action: func pointer
+ * @action: is a function pointer
  */
 void array_iterator(int *array, size_t size, void (*action(int)))
 {
 	unsigned int i;
 
+	if (!array || !action)
+		return
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
