b070274e58c289a422870d0c2526762cfe109829
El-Zieny <abdoelzieny72@gmail.com>
2023-04-20 17:35:22 -0700

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,14 +6,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (!n)
-		return (0);
 	va_list ap;
 	unsigned int i, res;
 
+	if (!n)
+		return (0);
 	va_start(ap, n);
+	res = 0;
 	for (i = 0; i < n; i++)
 		res += va_arg(ap, int);
 	va_end(ap);
-	return(res);
+	return (res);
 }
