0dced885f469acb78a155abd9e2a7b49cda73cc0
El-Zieny <abdoelzieny72@gmail.com>
2023-04-21 15:59:12 -0700

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(str, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i)
+		if (i && separator)
 			printf("%s", separator);
 		printf("%s", va_arg(str, char*));
 	}
