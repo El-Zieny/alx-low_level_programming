f751115135b24ca561b27c8436e9808f51a0ccbe
El-Zieny <abdoelzieny72@gmail.com>
2023-04-26 08:25:04 -0700

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,11 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(dog));
 	if (!d)
 		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	free(d);
 }
