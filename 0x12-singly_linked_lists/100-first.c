cfa469a662f9bef41e33864cda9af20533ef8b26
El-Zieny <abdoelzieny72@gmail.com>
2023-04-27 19:42:50 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void myStartupFun (void) __attribute__ ((constructor));
+/**
+ * before_main - prints before main function exucuted
+ */
+void before_main (void)
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
