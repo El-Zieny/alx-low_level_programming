0dfefd510c861c22c84c56e9741eb4a1da4686c3
El-Zieny <abdoelzieny72@gmail.com>
2023-04-27 17:46:11 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,18 @@ size_t print_list(const list_t *h)
 {
 	unsigned int x, i, c;
 
-	x = 0, c = 0;
-	if (!(h->str))
-		printf("[0] (nil)\n"), x++, c++;
-	else
-		printf("[%u] %s\n", h->len, h->str), x++;
-	if (h->next)
+	if (h)
 	{
-		i = x;
-		print_list(h->next), i++;
+		x = 0, c = 0;
+		if (!(h->str))
+			printf("[0] (nil)\n"), x++, c++;
+		else
+			printf("[%u] %s\n", h->len, h->str), x++;
+		if (h->next)
+		{
+			i = x;
+			print_list(h->next), i++;
+		}
 	}
 	return (i);
 }
