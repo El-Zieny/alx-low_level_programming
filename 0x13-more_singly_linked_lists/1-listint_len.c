ae29336210d8c9f6392f65287d7acf1a1ec1a749
El-Zieny <abdoelzieny72@gmail.com>
2023-04-30 08:28:55 -0700

    1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-/** listint_len - counts the elements of a node
+/**
+ * listint_len - counts the elements of a node
  * @h: the hed of the linked list
  * Return: the result
  */
@@ -7,8 +8,6 @@ size_t listint_len(const listint_t *h)
 {
 	size_t x;
 
-	if (!h)
-		return (-1);
 	x = 0;
 	while (h)
 	{
