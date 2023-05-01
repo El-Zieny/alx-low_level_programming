6309d2305d5ce17a132563f09db62a1d56a03c73
El-Zieny <abdoelzieny72@gmail.com>
2023-04-30 18:38:35 -0700

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees the whole lisnt (head included)
+ * @head: the head of the linst
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	node = head->next;
+	head->next = NULL;
+	while (head->next)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+	free(head);
+}
