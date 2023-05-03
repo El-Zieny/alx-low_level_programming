b87a7a2deddb670da57a977d2c9836d2faa074c0
El-Zieny <abdoelzieny72@gmail.com>
2023-05-02 18:37:06 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,13 +7,25 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *del;
+	listint_t *del, *node;
 	unsigned int x;
 
-	del = *head;
-	for (x = 0; x <= index; x++)
+	if (!*head)
+		return (-1);
+	if (index == 0)
 	{
-		if 
+		*head = (*head)->next;
+		return (1);
+	}
+	del = *head;
+	for (x = 0; x < index - 1; x++)
+	{
+		if (!del->next)
+			return (-1);
 		del = del->next;
-
+	}
+	node = del->next;
+	del->next = node->next;
+	free(node);
+	return (1);
 }
