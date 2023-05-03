e2e008c3da19f76f4c1a9b1bb0c24afa61a251bf
El-Zieny <abdoelzieny72@gmail.com>
2023-05-02 19:08:48 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,23 +19,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	if (!idx)
 	{
-		new->next = *head;
+		new->next = (*head)->next;
 		*head = new;
+		return (new);
 	}
 	tmp = *head;
 	for (x = 0; x < idx; x++)
 	{
-		/*
-		 * if (!tmp->next && x == idx - 1)
+		if (tmp->next == (NULL) && x = idx - 1)
 		{
 			tmp->next = new;
 			new->next = NULL;
 			return (new);
-		}*/
-		if (x == idx - 1)
-			break;
-		if (tmp->next == NULL)
-			return (NULL);
+		}
+		if(tmp->next == NULL)
+			return (NULL)
 		tmp = tmp->next;
 	}
 	new->next = tmp->next;
