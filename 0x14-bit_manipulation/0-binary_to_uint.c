22d2e99eac37af788303bbdc4b84c4231f75219d
El-Zieny <abdoelzieny72@gmail.com>
2023-05-04 17:01:48 -0700

    9

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _pow - power
+ * @base: the base number
+ * @power: the power number
+ * Rteurn: the result
+ */
+
+unsigned int _pow(unsigned int base, unsigned int power)
+{
+	unsigned int x;
+
+	if (!power)
+		return (1);
+	x = 1;
+	while (power)
+	{
+		x = x * base;
+		power--;
+	}
+	return (x);
+}
+/**
+ * binary_to_uint - converts a binary string to an unsigned int
+ * @b: the string of the binary number
+ * Rteurn: the uint result of the conversion or 0 if fail
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x, i;
+	int c;
+
+	if (!b)
+		return (0);
+	x = strlen(b);
+	c = x - 1;
+	i = 0;
+	while (c >= 0)
+	{
+		if (b[c] == '1')
+			i += _pow(2, (x - 1 ) - c);
+		else if (b[c] != '0')
+			return (0);
+		c--;
+	}
+	return (i);
+}
