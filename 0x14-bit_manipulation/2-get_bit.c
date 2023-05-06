8f9d2509ef8b75989bf0fdba6849b280c1d4b20f
El-Zieny <abdoelzieny72@gmail.com>
2023-05-05 19:21:34 -0700

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,12 +3,14 @@
  * get_bit - yes
  * @n: the bit holder
  * @index: of the bit
- * Return the value of the bit
+ * Return: the value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int x = ((n >> index) & 1);
 
+	if (index > 63)
+		return (-1);
 	if (x == 1)
 		return (1);
 	else if (x == 0)
