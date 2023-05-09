292d00d1f33669550b85b4cdd24c2c35baf27590
El-Zieny <abdoelzieny72@gmail.com>
2023-05-09 16:45:26 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,15 +8,16 @@
 int create_file(const char *filename, char *text_content)
 {
 	FILE *fd;
-
+	unsigned int c;
 	if (!filename)
 		return (-1);
 	fd = fopen(filename, "w");
 	if (!fd)
 		return (-1);
-	fwrite(text_content, 1, strlen(text_content), fd);
+	c = fwrite(text_content, 1, strlen(text_content), fd);
+	if (c != strlen(text_content))
+			return (-1);
 	fclose(fd);
-	if (chmod(filename, S_IRUSR | S_IWUSR))
-		return (-1);
+	chmod(filename, S_IRUSR | S_IWUSR);
 	return (1);
 }
