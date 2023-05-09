ee3a1e012d97a927e7891687417a0162d7a737cc
El-Zieny <abdoelzieny72@gmail.com>
2023-05-08 19:28:57 -0700

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * read_textfile - check the code
+ * @filename: yes
+ * @letters: the number of letters to be read
+ * Return: the actual number of letters that read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *read;
+	FILE* fd;
+
+	fd = fopen(filename, "r");
+	if (!fd)
+		return 0;
+	read = malloc(sizeof(char) * letters + 1);
+	if (!read)
+		return (0);
+	letters = fread(read, 1, letters, fd);
+	read[letters] = '\0';
+	printf("%s", read);
+	free(read);
+	fclose(fd);
+	return (letters);
+}
