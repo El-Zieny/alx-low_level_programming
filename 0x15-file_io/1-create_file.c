7ed93664a961243e0bb928094a9979e70e3b9b62
El-Zieny <abdoelzieny72@gmail.com>
2023-07-11 23:31:21 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,13 +7,13 @@
  */
 int create_file(const char *filename, char *text)
 {
-	int o ,w;
+	int o, w;
 
 	if (!filename)
 		return (-1);
 
-	o = open(filename, O_RDWR);
-	w = write(o, !text ? "" : text, !text ? 0 : strlen(text);
+	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	w = write(o, !text ? "" : text, !text ? 0 : strlen(text));
 
 	if (o < 0 || w < 0)
 		return (-1);
