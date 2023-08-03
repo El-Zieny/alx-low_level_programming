8b64d7bf6b33295efc0e062a2bc18f7633fc86b2
El-Zieny <abdoelzieny72@gmail.com>
2023-08-03 10:26:35 +0300

    printf >> _putchar

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,17 @@ void print_binary(unsigned long int n)
 {
 	if (!n)
 	{
-		printf("0");
+		_putchar('0');
 		return;
 	}
 	if (n == 1)
 	{
-		printf("1");
+		_putchar('1');
 		return;
 	}
 	print_binary(n >> 1);
 	if (n & 1)
-		printf("1");
+		_putchar('1');
 	else
-		printf("0");
+		_putchar('0');
 }
