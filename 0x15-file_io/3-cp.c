985daeb9424aa91ab058e6dca1a9ffc85296f363
El-Zieny <abdoelzieny72@gmail.com>
2023-08-08 00:14:27 +0300

    task #3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * main -  a program that copies the content of a file to another file.
+ * @ac: arguments counter
+ * @av: arguments vector
+ * Return: 0 if success
+ */
+int main(int ac, char **av)
+{
+	int o, r, w, c;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	o = open(av[1],O_RDONLY);
+	r = read(o, buffer, 1024);
+	if (r < 0)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	c = close(o);
+        if (c < 0)
+	{
+		fprintf(stderr, "Error: Can't close fd %i\n", o);
+		exit(100);
+	}
+
+	o = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	w = write(o, buffer, r);
+	if (w < 0)
+	{
+		fprintf(stderr, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	c = close(o);
+	if (c < 0)
+	{
+		fprintf(stderr, "Error: Can't close fd %i\n", o);
+		exit(100);
+	}
+	return (0);
+}
