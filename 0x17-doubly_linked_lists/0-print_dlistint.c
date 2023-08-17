07a23cab423fb344cfdfebf2fe0761efe61c1d88
abdulrahman elzieny <abdoelzieny72@gmail.com>
2023-08-17 11:07:32 -0700

    Task #0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,13 +4,13 @@
  * @h: the head of the list
  * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	int x = 0;
 
 	while (h)
 	{
-		printf(h->n);
+		printf("%i\n", h->n);
 		h = h->next;
 		x++;
 	}
