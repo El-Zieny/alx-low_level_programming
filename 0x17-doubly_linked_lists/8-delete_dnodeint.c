c63889bc24d610a3bbbffea0b8b851ed1ff7f7b7
abdulrahman elzieny <abdoelzieny72@gmail.com>
2023-08-17 18:59:16 -0700

    Task #8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - yes
+ * @head: of the dlist
+ * @index: of the node to be deleted
+ * Return: 1 if succeeded, -1 if failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	size_t len = dlistint_len(*head);
+	dlistint_t *tmp;
+
+	if (index > len || !(*head))
+		return (-1);
+	tmp = *head;
+	if (!len && !index)
+		*head = NULL;
+	else if (!index)
+	{
+		(*head)->next->prev = NULL;
+		(*head) = (*head)->next;
+	}
+	else if (len == index)
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->prev->next = NULL;
+	}
+	else
+	{
+		while (index)
+		{
+			tmp = tmp->next;
+			index--;
+		}
+		tmp->prev->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+	}
+	free(tmp);
+	return (1);
+}
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint
+ * @h: the head of the dlistint
+ * Return: the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		x++;
+		h = h->next;
+	}
+
+	return (x);
+}
