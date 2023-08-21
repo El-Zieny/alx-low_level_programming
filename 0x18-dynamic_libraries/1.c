9a4106ff4cb56c03426ddf482a24e768dded718a
El-Zieny <abdoelzieny72@gmail.com>
2023-08-21 20:30:53 +0300

    Task #0

---
+++ 0x18-dynamic_libraries/1.c
@@ -0,0 +1,20 @@
+int _putchar(char c){(void) c;}
+int _islower(int c){(void) c;}
+int _isalpha(int c){(void) c;}
+int _abs(int n){(void) n;}
+int _isupper(int c){(void) c;}
+int _isdigit(int c){(void) c;}
+int _strlen(char *s){(void) s;}
+void _puts(char *s){(void) s;}
+char *_strcpy(char *dest, char *src){(void) dest; (void) src;}
+int _atoi(char *s){(void) s;}
+char *_strcat(char *dest, char *src){(void) dest; (void) src;}
+char *_strncat(char *dest, char *src, int n){(void) dest; (void) src; (void) n;}
+char *_strncpy(char *dest, char *src, int n){(void) dest; (void) src; (void) n;}
+int _strcmp(char *s1, char *s2){(void) s1; (void) s2;}
+char *_memset(char *s, char b, unsigned int n){(void) b; (void) n; (void) s;}
+char *_memcpy(char *dest, char *src, unsigned int n){(void) dest; (void) src; (void) n;}
+char *_strchr(char *s, char c){(void) s; (void) c;}
+unsigned int _strspn(char *s, char *accept){(void) accept; (void) s;}
+char *_strpbrk(char *s, char *accept){(void) accept; (void) s;}
+char *_strstr(char *haystack, char *needle){(void) needle; (void) haystack;}
