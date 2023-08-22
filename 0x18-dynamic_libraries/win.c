cbe412ea3954cb50eecb1c0b1e1d4e7de09f438c
El-Zieny <abdoelzieny72@gmail.com>
2023-08-22 05:14:06 +0300

    Task #3

--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 int printf(const char *format, ...)
 {
-	printf("9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!");
+	write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 55);
 	exit(EXIT_SUCCESS);
 }
