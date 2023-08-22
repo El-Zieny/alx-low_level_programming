483f9986e88e9f8fdcc9bbe206916b1f0473f682
El-Zieny <abdoelzieny72@gmail.com>
2023-08-22 20:48:59 +0300

    Task #3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -s -o https://github.com/El-Zieny/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=./win.so
+curl -s -o ~/win.so https://github.com/El-Zieny/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=~/win.so
