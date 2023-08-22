abeaa5f4dd2dc252446dc9c3bf6be047dd197fe1
El-Zieny <abdoelzieny72@gmail.com>
2023-08-22 03:06:01 +0300

    Task #3

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+LD_PRELOAD=./win.so ./gm 9 8 10 24 75 9
---
+++ 0x18-dynamic_libraries/win.c
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+bool check_input(int a1[], int a2, int a3)
+{
+	if (a1[0] == 9 && a1[1] == 8 && a1[2] == 10)
+		if (a1[3] == 24 && a1[4] == 75 && a1[5] == 9)
+			return (1);
+}
