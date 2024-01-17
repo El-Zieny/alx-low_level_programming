bae6477986d6703385a29eb8fc8f2a15d34e9e55
El-Zieny <abdoelzieny72@gmail.com>
2024-01-17 18:06:23 +0200

    0x1E-search_algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
-#ifndef _SEARCH_ALGORITHM_
-#define _SEARCH_ALGORITHM_
-#include <stdlib.h>
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
 #include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
+
 #endif
